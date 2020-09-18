c623e2d47f907aa9b4a5dabc63612ffa0ba28651
ollie <ollie@place.technology>
2020-09-18 22:22:05 +1000

    Added users profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,13 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         @articles = @user.articles
     end
+
     def new
         @user = User.new
+    end
+
+    def index
+        @users = User.all
     end
 
     def edit
