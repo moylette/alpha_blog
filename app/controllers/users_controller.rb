d7c5434331f8a5a27d137d55eeb07c2473f4f129
ollie <ollie@place.technology>
2020-09-19 14:29:31 +1000

    Added user login actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
+
+    before_action :set_user, only: [:show, :edit, :update]
+
     def show
-        @user = User.find(params[:id])
         @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
 
@@ -13,11 +15,10 @@ class UsersController < ApplicationController
     end
 
     def edit
-        @user = User.find(params[:id])
+
     end
 
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "Your account was successfully updated"
             redirect_to @user5
@@ -30,8 +31,9 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            flash[:notice] = "Welcome to the Alpha Blog #{ @user.username }. You have successfully signed up"
-            redirect_to articles_path
+          session[:user_id] = @user.id
+          flash[:notice] = "Welcome to the Alpha Blog #{ @user.username }. You have successfully signed up"
+          redirect_to articles_path
         else
             render 'new'
         end
@@ -40,5 +42,9 @@ class UsersController < ApplicationController
     private 
     def user_params
       params.require(:user).permit(:username, :email, :password)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home'
+
   get 'about', to: 'pages#about'
+
   resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   #The above line can be achieved with 'resources :articles' as all of the restful actions have been exposed
+
   get 'signup', to: 'users#new'
+
   resources :users, except: [:new]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
 end
