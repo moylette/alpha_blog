6ef1bc4d90f614fd94f44357913d7bd0ac51145c
ollie <ollie@place.technology>
2020-09-20 14:28:17 +1000

    Added delete user and associated articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 
-    before_action :set_user, only: [:show, :edit, :update]
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
     before_action :require_user, only: [:edit, :update]
-    before_action :require_same_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
 
     def show
         @articles = @user.articles.paginate(page: params[:page], per_page: 5)
@@ -39,6 +39,13 @@ class UsersController < ApplicationController
         else
             render 'new'
         end
+    end
+
+    def destroy
+      @user.destroy
+      session[:user_id] = nil
+      flash[:notice] = "Account and associated articles deleted"
+      redirect_to articles_path
     end
 
     private 
