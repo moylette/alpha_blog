0445afba3ad5127ecdaecee94e8cca197cff606e
ollie <ollie@place.technology>
2020-09-22 22:55:07 +1000

    Added categories many to many association with articles and the ui option to add and edit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_20_115139) do
+ActiveRecord::Schema.define(version: 2020_09_22_111510) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
